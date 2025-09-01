51f909f134c9a522574dbace14c631d8b1ed9298
sonnet <22-46766-1@student.aiub.edu>
2025-09-01 14:08:48 +0600

    updated

--- FinanceTracker/Controller/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-setcookie('status', '', time() - 3600, '/'); // Delete the cookie
-session_destroy(); // Destroy the session
-header('location: ../View/login.php');
-exit();
-?>
